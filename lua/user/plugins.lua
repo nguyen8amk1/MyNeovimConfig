b0d88b49bc45329ce81cddf01dce5cf54efde7dd
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-11-30 22:00:04 +0700

    add completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -23,6 +23,17 @@ require("lazy").setup({
         lazy = false,
         priority = 1000,
         opts = {},
-    }
+    }, 
+
+        -- cmp plugins
+    { "hrsh7th/nvim-cmp"},  -- The completion plugin
+    { "hrsh7th/cmp-buffer"}, -- buffer completions
+    { "hrsh7th/cmp-path"},  -- path completions
+    { "hrsh7th/cmp-cmdline"},  -- cmdline completions
+    { "saadparwaiz1/cmp_luasnip"},  -- snippet completions
+
+    -- snippets
+    { "L3MON4D3/LuaSnip"},  --snippet engine
+    { "rafamadriz/friendly-snippets"},  -- a bunch of snippets to 
 })
 
