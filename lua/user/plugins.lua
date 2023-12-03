4b74c0962ab7869dd3b89049edf3c635d27744a8
nguyen8amk1 <trungnguyen123akohshit@gmail.com>
2023-12-03 10:25:53 +0700

    Neotree works great

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,18 @@ require("lazy").setup({
     -- Nvim tree  
     { 'kyazdani42/nvim-web-devicons'}, 
     { 'kyazdani42/nvim-tree.lua'}, 
+    { 's1n7ax/nvim-window-picker'}, 
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }, 
+    }, 
 })
 
 
