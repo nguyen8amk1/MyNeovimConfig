c636d38ac66da1248031ba0884230099e695b38b
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-01-28 16:25:24 +0700

    move neo-tree to separate file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -194,16 +194,6 @@ return {
     build = ':TSUpdate',
   },
 
-  {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-          "MunifTanjim/nui.nvim",
-          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-      }, 
-  }, 
   { 's1n7ax/nvim-window-picker'},
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
