47a32145da920343d838a21092006f1c7bb780ef
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-02-23 22:54:55 +0700

    not done yet, will look to nvim-treesitter-context later

--- init.lua
+++ init.lua
@@ -194,6 +194,7 @@ require('lazy').setup({
   },
 
   { "alexghergh/nvim-tmux-navigation"},
+  { "nvim-treesitter/nvim-treesitter-context" }, 
 
   { "Mofiqul/dracula.nvim" },
   -- {
@@ -1355,3 +1356,18 @@ dracula.setup({
 
 
 vim.cmd.colorscheme 'dracula'
+
+require'treesitter-context'.setup{
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 20, -- The Z-index of the context window
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+}
