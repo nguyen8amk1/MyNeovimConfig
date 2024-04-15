d011fd22b37a0621da59f4a0c21a5bb57bc2f6eb
Nguyễn Trần Trung Nguyên <trungnguyen123akohshit@gmail.com>
2024-04-15 20:26:24 +0700

    starting point

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 --[[
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -1081,3 +1082,7 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --
+=======
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+>>>>>>> Stashed changes
